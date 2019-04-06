1f8e68275a484e1a20822bcd1d536ddc0ba873f0
Harshit Gindra <harshitgindra@gmail.com>
2019-04-06 23:55:19 +0000

    Initial commit

---
+++ SplashScreen.Xamarin/SplashScreen.Android/SplashActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            try
+            {
+                base.OnResume();
+                var intent = new Intent(this, typeof(MainActivity));
+                if (Intent.Extras != null)
+                {
+                    intent.PutExtras(Intent.Extras);
+                }
+                StartActivity(intent);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+    }
+}
